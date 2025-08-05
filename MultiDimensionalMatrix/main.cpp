9f981a43cec08c1106e91652b56958fb146d0993
Davit Melikyan <davidmelikyan900@gmail.com>
2025-08-05 13:09:55 +0400

    Templates

--- MultiDimensionalMatrix/main.cpp
+++ MultiDimensionalMatrix/main.cpp
@@ -5,18 +5,23 @@
 void square(int& x) { x = x * x; }
 
 int main() {
-    MultiDimMatrix<int, 3> matrix({2, 3, 4}, 1); // 2x3x4 matrix filled with 1s
+    MultiDimMatrix<int, 3> mat1({2, 3, 4}, 1);
+    mat1[0][1][2] = 42;
+    mat1[1][2][3] = 5;
+    std::cout << "[0][1][2]: " << mat1[0][1][2] << std::endl;
+    std::cout << "Sum: " << mat1.sum() << "\n";
+    std::cout << "Min: " << mat1.min() << "\n";
+    std::cout << "Max: " << mat1.max() << "\n";
+    mat1.apply(square);
+    std::cout << "After squaring [1][2][3] = " << mat1[1][2][3] << std::endl;
 
-    matrix[0][1][2] = 42;
-    matrix[1][2][3] = 5;
-
-    std::cout << "Value at [0][1][2]: " << matrix[0][1][2] << "\n";
-    std::cout << "Sum: " << matrix.sum() << "\n";
-    std::cout << "Min: " << matrix.min() << "\n";
-    std::cout << "Max: " << matrix.max() << "\n";
-
-    matrix.apply(square);
-    std::cout << "After squaring, [1][2][3] = " << matrix[1][2][3] << "\n";
+    MultiDimMatrix<double, 3> mat2 ({3,3,4}, 4.5);
+    mat2[0][1][2] = 2.5;
+    mat2[1][2][3] = 10.98;
+    std::cout << "[0][1][2]: " << mat2[0][1][2] << std::endl;
+    std::cout << "Sum: " << mat2.sum() << "\n";
+    std::cout << "Min: " << mat2.min() << "\n";
+    std::cout << "Max: " << mat2.max() << "\n";
 
     return 0;
 }
